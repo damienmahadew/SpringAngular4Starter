5dda76582320f16bf5dd107c59e1bde7a186c4f4
damien.mahadew <damien.mahadew@gmail.com>
2017-06-29 15:10:40 +0200

    base project

---
+++ spring-angular4-starter-web/src/main/java/za/co/spring/angular4/starter/SpringAngular4StarterWebApplication.java
@@ -0,0 +1,12 @@
+package za.co.spring.angular4.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngular4StarterWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAngular4StarterWebApplication.class, args);
+	}
+}
---
+++ spring-angular4-starter-web/src/test/java/za/co/spring/angular4/starter/SpringAngular4StarterWebApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.spring.angular4.starter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAngular4StarterWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
